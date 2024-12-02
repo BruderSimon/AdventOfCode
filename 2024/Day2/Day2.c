20a86acbd2e71ad2260733bfcdc4780649249c46
Simon Engel <engesi02@thu.de>
2024-12-02 21:22:30 +0100

    Day 2

---
+++ 2024/Day2/Day2.c
@@ -0,0 +1,68 @@
+/**
+ * My solutions to Advent of Code
+ * https://adventofcode.com/
+ *
+ * Day 2, 2024
+ * @author: Simon Engel
+ */
+
+#include <stdio.h>
+
+int calculateReport(int* report, int inserts)
+{
+  int dec = 0;
+  int inc = 0;
+  for(int i = 1; i <= inserts; i++){
+    printf("I: %d \n", report[i]);
+    if(report[i-1] < report[i]){
+      if(dec)
+	return 0;
+      inc = 1;
+    }else if (report[i-1] > report[i]){
+      if(inc)
+	return 0;
+      dec = 1;
+    }else{
+      return 0;
+    }
+    int temp = (report[i-1] - report[i]);
+    if(temp < 0)
+      temp *= -1;
+    printf("Temp: %d\n", temp);
+    if(temp < 1 || temp >3)
+      return 0;
+  }
+  
+  return 1;
+}
+
+int main()
+{
+  FILE* fptr;
+  errno_t err = fopen_s(&fptr, "input.txt", "r");
+    
+  if(err != 0) {
+    printf("Error while opening file: input.txt\n");
+    return 1;
+  }
+  char line[256];
+  int result = 0;
+  while (fgets(line, sizeof(line), fptr)){
+    int current_number = 0;
+    int report[256];
+    int inserts = 0;
+    for (int i = 0;  line[i] != '\n'; i++) {
+      if (line[i] >= '0' && line[i] <= '9') {
+	current_number = current_number * 10 + (line[i] - '0');
+      } else if (line[i] == ' ') {
+	report[inserts++] = current_number;
+	current_number = 0;
+      }
+      /* printf("i: %d \n", i); */
+      /* printf("current_number: %d \n", current_number); */
+    }
+    result += calculateReport(report, inserts);
+    /* printf("Intermediate resulut: %d \n", result); */
+  }
+  printf("Result is: %d \n", result);
+}
