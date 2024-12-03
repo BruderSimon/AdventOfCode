5e2eb75c1aad4bf995b9f7b2c2ad4d2c62d39fb8
SimonE <simon.engel@de.zuken.com>
2024-12-03 13:55:21 +0100

    Worked further on Part2

--- 2024/Day2/Day2.c
+++ 2024/Day2/Day2.c
@@ -4,6 +4,9 @@
  *
  * Day 2, 2024
  * @author: Simon Engel
+ *
+ * Part Two:
+ * A report is save as long as it
  */
 
 #include <stdio.h>
@@ -12,8 +15,7 @@ int calculateReport(int* report, int inserts)
 {
   int dec = 0;
   int inc = 0;
-  for(int i = 1; i <= inserts; i++){
-    printf("I: %d \n", report[i]);
+  for(int i = 1; i < inserts; i++){
     if(report[i-1] < report[i]){
       if(dec)
 	return 0;
@@ -28,10 +30,45 @@ int calculateReport(int* report, int inserts)
     int temp = (report[i-1] - report[i]);
     if(temp < 0)
       temp *= -1;
-    printf("Temp: %d\n", temp);
     if(temp < 1 || temp >3)
       return 0;
   }
+  
+  return 1;
+}
+
+int calculateReport2(int* report, int inserts)
+{
+  int dec = 0;
+  int inc = 0;
+  int goodLevel[256];
+  int goodLevels = 0;
+  
+  for(int i = 1; i < inserts; i++){
+    if(report[i-1] < report[i]){
+      if(dec){
+        
+      }
+      return 0;
+      inc = 1;
+    }else if (report[i-1] > report[i]){
+      if(inc)
+	return 0;
+      dec = 1;
+    }else{
+      return 0;
+    }
+    int temp = (report[i-1] - report[i]);
+    if(temp < 0)
+      temp *= -1;
+    if(temp < 1 || temp >3)
+      return 0;
+  }
+ 
+  if((inserts - goodLevels) > 1)
+    return 0;
+  else if ((inserts - goodLevels) > 0)
+    return calculateReport(goodLevel, goodLevels);
   
   return 1;
 }
@@ -47,6 +84,7 @@ int main()
   }
   char line[256];
   int result = 0;
+  int result2 = 0;
   while (fgets(line, sizeof(line), fptr)){
     int current_number = 0;
     int report[256];
@@ -58,11 +96,12 @@ int main()
 	report[inserts++] = current_number;
 	current_number = 0;
       }
-      /* printf("i: %d \n", i); */
-      /* printf("current_number: %d \n", current_number); */
     }
+    report[inserts++] = current_number;
     result += calculateReport(report, inserts);
-    /* printf("Intermediate resulut: %d \n", result); */
+    result2 += calculateReport2(report, inserts);
+    
   }
-  printf("Result is: %d \n", result);
+  printf("Result for Part 1 is: %d \n", result);
+  printf("Result for Part 2 is: %d \n", result2);
 }
