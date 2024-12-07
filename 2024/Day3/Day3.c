b20f7bd3f7ae4267764419b6e1c1ebef81a8c106
Simon Engel <engesi02@thu.de>
2024-12-07 14:34:30 +0100

    Day3 Part 1

--- 2024/Day3/Day3.c
+++ 2024/Day3/Day3.c
@@ -9,29 +9,67 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void part1(FILE* fptr){
+  
+  int character;
+  int result = 0;
+  char* mul = "mul(,)";
+  int count = 0;
+  int current_number = 0;
+  int add = 1;
+    
+  while ((character = fgetc(fptr)) != EOF){
+    char c = (char) character;
+    
+    if(count >= 4){
+      if (c >= '0' && c <= '9') {
+	if((current_number/100) < 10)
+	  current_number = current_number * 10 + (c - '0');
+	else{
+	  current_number = 0;
+	  count = 0;
+	  add = 1;
+	}
+      }else if(c == ','){
+
+	add *= current_number;
+	current_number = 0;
+	count++;
+	
+      }else if(c == ')'){
+	add *= current_number;
+	result += add;
+	current_number = 0;
+	count = 0;
+	add = 1;
+      }else{
+	current_number = 0;
+	count = 0;
+	add = 1;
+      }
+    }else{
+      if(c == mul[count]){
+	count++;
+      }else{
+	count = 0;
+      }
+    }
+  }
+  printf("Result: %d \n", result);
+}
+
 
 int main()
 {
-   FILE* fptr;
+  FILE* fptr;
   errno_t err = fopen_s(&fptr, "input.txt", "r");
     
   if(err != 0) {
     printf("Error while opening file: input.txt\n");
     return 1;
   }
-  char line[256];
-  int result = 0;
-  int result2 = 0;
-  while (fgets(line, sizeof(line), fptr)){
-    int current_number = 0;
-    int report[256];
-    int inserts = 0;
-    for (int i = 0;  line[i] != '\n'; i++) {
-      if (line[i] >= '0' && line[i] <= '9') {
-	
-      }
-    }
-    
-  }
-  
+
+  part1(fptr);
 }
