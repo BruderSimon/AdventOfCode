395d7bab54d72e4ee936b8655a0b9650ff1f626d
SimonE <simon.engel@de.zuken.com>
2024-12-18 16:56:47 +0100

    Got Day5 part 1 working, but stupid me need to refactor the code to mae Part 2 easier

--- 2024/Day5/Day5.c
+++ 2024/Day5/Day5.c
@@ -24,77 +24,82 @@ int containsAfter(struct rule* r, int check)
     if(r->after[i] == check)
       return 1;
   }
-    return 0;
+  return 0;
 };
-
-void part1(FILE* fptr){
-  
-  int character;
-  int result = 0;
+int* getNumbers(char* line)
+{
+  int* arr = calloc(100, sizeof(int));
+  int count = 0;
   int current_number = 0;
-  struct rule rules[100];
-  int* update = (int*) calloc(256, sizeof(int));
-  struct rule* ruleptr = NULL;
+  for (int i = 0;  line[i] != '\n' && line[i] != '\0'; i++)
+    {
+      if (line[i] >= '0' && line[i] <= '9')
+	{
+	  current_number = current_number * 10 + (line[i] - '0');
+	}
+      else
+	{
+	  arr[count++] = current_number;
+	  current_number = 0;
+	}
+    }
+  return arr;
+}
+void addRule(char* line, struct rule* rules)
+{
+  int* num = getNumbers(line);
+  rules[num[0]].after[num[1]] = num[1];
+  free(num);
+}
+int part1(char* line, struct rule* rules){
   
-  char line[256];
-  int isUpdate = 0;
-  int updateNums = 0;
-  while (fgets(line, sizeof(line), fptr)){
-    for (int i = 0;  line[i] != '\n'; i++) {
-      if (line[i] >= '0' && line[i] <= '9'){
-	current_number = current_number * 10 + (line[i] - '0');
-      }else if (line[i] == '|'){
-	ruleptr = &rules[current_number];
-	ruleptr->num = current_number;
-	current_number = 0;
-      }else if (line[i] == ','){
-	isUpdate = 1;
-	update[updateNums++] = current_number;
-	current_number = 0;
-      }
+  int* update = getNumbers(line);
+
+  int isGoodUpdate = 0;
+  int count = 0;
+  for(int i = 1; update[i] != 0; i++)
+    {
+      for(int d = i-1; d >= 0 && i > 0; d--)
+	{
+	  if(&rules[update[i]] == NULL)
+	    break;
+	  if(containsAfter(&rules[update[i]], update[d]))
+	    {
+	      free(update);
+	      return 0;
+	    }
+	}
+      count = i;
     }
-    if(current_number != 0){
-      if(!isUpdate){
-	ruleptr->after[current_number] = current_number;
-	ruleptr = NULL;
-      }else{
-	update[updateNums++] = current_number;
-      }
-    }
-    current_number = 0;
-    if(isUpdate){
-      int isGoodUpdate = 0;
-      for(int i = updateNums-1; i >= 0; i--){
-	 for(int d = i-1; d >= 0 && i > 0; d--){
-	   if(&rules[update[i]] == NULL)
-	     break;
-	   if(containsAfter(&rules[update[i]], update[d])){
-	     isGoodUpdate = 0;
-	     break;
-	   }else{
-	     isGoodUpdate = 1;
-	   }
-	 }
-      }
-      if(isGoodUpdate){
-	result += update[updateNums/2];
-      }
-    }
-  }
-  printf("Result: %d \n", result);
-  free(update);}
+  free(update);
+  return update[count/2];
+      
+}
 
 int main()
 {
   FILE* fptr;
-  errno_t err = fopen_s(&fptr, "input.txt", "r");
+  errno_t err = fopen_s(&fptr, "Test.txt", "r");
     
   if(err != 0) {
     printf("Error while opening file: input.txt\n");
     return 1;
   }
-  part1(fptr);
 
-    
+  int result1 = 0;
+  char line[256];
+  struct rule rules[100];
+  int isUpdate = 0;
+   while (fgets(line, sizeof(line), fptr))
+    {
+      if(!isUpdate)
+	addRule(line, rules);
+
+      if(line[0] == '\n')
+	isUpdate = 1;
+	
+      result1 += part1(line, rules);
+    }
+   printf("Result Part 1: %d \n", result1);
   fclose(fptr);
 }
